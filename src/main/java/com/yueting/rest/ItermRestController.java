91c9366c2292df43c32e479645823aa08048cd26
man-v2 <yeaskyone@126.com>
2018-06-27 20:11:37 +0800

    add query iterms list

--- src/main/java/com/yueting/rest/ItermRestController.java
+++ src/main/java/com/yueting/rest/ItermRestController.java
@@ -1,14 +1,19 @@
 package com.yueting.rest;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.yueting.Repository.ItermRepository;
 import com.yueting.model.Iterm;
+import com.yueting.util.MsgCode;
+import com.yueting.util.Result;
 
 @RestController
 @RequestMapping("/api/iterm")
@@ -27,7 +32,20 @@ public class ItermRestController {
 	}
 	
 	@GetMapping("/")
-	public Long count() {
-		return itermRepository.count();
+	public Result<List<Iterm>> count() {
+		Result<List<Iterm>> result = new Result<List<Iterm>>();
+		result.setCode(MsgCode.SUCCESS.getCode());
+		result.setMsg(MsgCode.SUCCESS.getDescription());
+		result.setData(itermRepository.findAll());
+		return result;
+	}
+	
+	@GetMapping(value="/{id}")
+	public Result<Iterm> getById(@PathVariable(value = "id") Integer id){
+		Result<Iterm> result = new Result<Iterm>();
+		result.setCode(MsgCode.SUCCESS.getCode());
+		result.setMsg(MsgCode.SUCCESS.getDescription());
+		result.setData(itermRepository.getOne(id));
+		return result;
 	}
 }
